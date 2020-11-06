6769df6f897a9f22416fb8244ff61a4b53753fb5
yasinakca <yasinakcadev@gmail.com>
2020-11-06 20:59:37 +0300

    managing the state for both checkbox & linethrough propert

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: Text('Item 1'),
-      trailing: Checkbox(
-        value: true,
-      ),
+      leading: Text('Item 1',style: TextStyle(decoration: isChecked == true ? TextDecoration.lineThrough : null),),
+      trailing: TaskCheckbox(isChecked,(value){
+        setState(() {
+          isChecked = value;
+        });
+      }),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckbox;
+
+  TaskCheckbox(this.checkboxState,this.toggleCheckbox);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckbox,
     );
   }
 }
